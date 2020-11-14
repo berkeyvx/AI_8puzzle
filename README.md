1c4224a025426652612d6c4857a1d1d552007f09
berkeyvx <iamberkeyavas@gmail.com>
2020-11-14 20:39:47 +0300

    changed array->pair for depth, implemented iterative deepening search, Depth Limited Search

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+<div align="center">
+
 # AI_8puzzle
+### Istanbul University - Cerrahpasa Computer Engineering Department 
+#### Artifical Intelligence TERM PROJECTS – Assignment 1 Fall 2020-2021
+
+</div>
+
+###### In this project you will use uninformed search methods to solve an 8-puzzle
+
+
+- Consists of a 3x3 board with 8 numbered tiles and a blank space.
+- A tile adjacent to the blank space can slide into the space.
+- The object is to reach a specified goal state, such as the one shown on the right.
+
+Implement an agent to solve an 8-puzzle with the following search methods: 
+1. Breadth-first
+2. Depth-first
+3. Depth limited (depth limit must be an argument, try different depth limits) 
+3. Iterative deepening search
+
+
+
+# HOW TO RUN
+###### Compile file, must add compiler flag -std=c++17
+**g++ -o m main.cpp -std=c++17**
+
+##### There is 3 option to run file. 
+1. Default one : **./m**
+2. Pass 1 Argument to Run DFS and BFS on your 8puzzle: **./m "\<board_state>**
+3. Pass 2 Argument to Run DFS,BFS,DLS and Iterative Deepening Search: **./m \<board_state> \<depth>**
+
+
--- main.cpp
+++ main.cpp
@@ -16,18 +16,19 @@
 #include <stack>
 
 class _8puzzle {
-    std::array<int, 9> currentState;
+    // std::pair<std::array<int, 9>, int depth>
+    std::pair<std::array<int, 9>, int> currentState;
     const std::array<int, 9> goalState {0,1,2,3,4,5,6,7,8};
     long long nodesExpanded = 0;
     long long maximumSizeOfFringe = 0;
     
     
-    std::array<int, 9> move(std::array<int, 9> currentState, int direction);
-    std::vector<std::array<int, 9>> moveAllDirection(std::array<int, 9> currentState);
+    std::pair<std::array<int, 9>, int> move(std::pair<std::array<int, 9>, int>, int direction);
+    std::vector<std::pair<std::array<int, 9>, int>> moveAllDirection(std::pair<std::array<int, 9>, int> currentState);
     std::string transformArrayToString(std::array<int,  9> arr);
     
-public:
-    _8puzzle():currentState{7,2,4,5,0,6,8,3,1}{
+public: // {7,2,4,5,0,6,8,3,1}
+    _8puzzle():currentState(std::make_pair(std::array<int, 9> {7,2,4,5,0,6,8,3,1}, 0)){
     }
     
     _8puzzle(const char * argv){
@@ -40,8 +41,10 @@ public:
         index = 0;
         
         for(; index < 9; ++index){
-            currentState[index] = argv[index] - '0';
+            currentState.first[index] = argv[index] - '0';
         }
+        
+        currentState.second = 0;
         
     }
     
@@ -50,18 +53,16 @@ public:
     
     void BFS();
     
-    // TODO:: implement Depth Limited Search
-    std::array<int, 9> DLS();
+    void DLS(int depth, bool IDS = false);
     
-    // TODO:: implement iterative deepening search
-    std::array<int, 9> IDS();
+    void IDS(int depth);
     
 
     void randomProblemGenerator();
     
     
     friend std::ostream &operator<<(std::ostream &output,const _8puzzle &puzz){
-        for (auto &ii : puzz.currentState) {
+        for (auto &ii : puzz.currentState.first) {
             output << ii << " ";
         }
         return output;
@@ -80,7 +81,27 @@ int main(int argc, const char * argv[]) {
         p.DFS();
     }
     */
- 
+    if(argc == 1){
+        _8puzzle p;
+        p.BFS();
+        p.DFS();
+        p.DLS(10);
+        p.DLS(30);
+        p.DLS(80);
+        p.IDS(20);
+        std::cout << "\n\n";
+        for (int i = 0; i <= 10; ++i) {
+            std::cout << "RANDOM PROBLEM NO: " << i << "\n";
+            p.randomProblemGenerator();
+            p.BFS();
+            p.DFS();
+            p.DLS(10);
+            p.DLS(30);
+            p.DLS(80);
+            p.IDS(20);
+            std::cout << "\n\n";
+        }
+    }
     
     if(argc == 2){
         _8puzzle p(argv[1]);
@@ -88,8 +109,13 @@ int main(int argc, const char * argv[]) {
         p.BFS();
     }
     if(argc == 3){
+        int depth = std::stoi(argv[2]);
+        
         _8puzzle p(argv[1]);
         p.BFS();
+        p.DFS();
+        p.DLS(depth);
+        p.IDS(depth);
     }
     
     return 0;
@@ -99,39 +125,44 @@ int main(int argc, const char * argv[]) {
 // -----------------------------------------PUBLIC MEMBER FUNCTIONS---------------------------------------------------
 
 void _8puzzle::BFS(){
-    std::deque<std::array<int, 9>> q;
+    std::deque<std::pair<std::array<int, 9>, int>> q; // queue for BFS
     std::unordered_set<std::string> set; // keep visited list
     bool solutionFind = false;
+    int depth = -1;
     
     q.push_front(this->currentState);
     
     while (!q.empty()) {
-        std::array<int, 9> m = q.back();
+        std::pair<std::array<int, 9>, int> m = q.back();
         q.pop_back();
         
-        set.insert(transformArrayToString(m));
-        if(std::equal(std::begin(m), std::end(m), std::begin(goalState))){
+        // check if goal state reached
+        set.insert(transformArrayToString(m.first));
+        if(std::equal(std::begin(m.first), std::end(m.first), std::begin(goalState))){
             solutionFind = true;
+            depth = m.second;
             break;
         }
         
+        // find next possible paths
         auto possiblePaths = moveAllDirection(m);
         
         // possible path is not in set so add in queue
         for(auto& i: possiblePaths){
-            std::string temp = transformArrayToString(i);
+            std::string temp = transformArrayToString(i.first);
             if(set.find(temp) == set.end()){
                 q.push_front(i);
-                set.insert(transformArrayToString(i));
+                set.insert(temp);
                 
             }
         }
         
+        // update maximum size of fridge if current stack size is bigger than previous
         if(this->maximumSizeOfFringe < q.size())
             maximumSizeOfFringe = q.size();
     }
     
-    std::cout << (solutionFind ? "Solution find for ": " Solution not find for ") << *this <<  " Nodes expanded: " << nodesExpanded << " Maximum Size of Fringe: " << maximumSizeOfFringe << "\n";
+    std::cout << (solutionFind ? "BFS Solution find for ": "BFS Solution not find for ") << *this <<  " Nodes expanded: " << nodesExpanded << " Maximum Size of Fringe: " << maximumSizeOfFringe << " Depth: " << depth << "\n";
     nodesExpanded = 0;
     maximumSizeOfFringe = 0;
 }
@@ -140,43 +171,116 @@ void _8puzzle::BFS(){
 
 
 void _8puzzle::DFS(){
-    std::stack<std::array<int, 9>> s;
+    std::stack<std::pair<std::array<int, 9>, int>> s; // stack for DFS
+    std::unordered_set<std::string> set; // keep visited list
+    bool solutionFind = false;
+    int depth = -1;
+    
+    s.push(this->currentState);
+    
+    while (!s.empty()) {
+        std::pair<std::array<int, 9>, int> m = s.top();
+        s.pop();
+        
+        // check if goal state reached
+        set.insert(transformArrayToString(m.first));
+        if(std::equal(std::begin(m.first), std::end(m.first), std::begin(goalState))){
+            solutionFind = true;
+            depth = m.second;
+            break;
+        }
+        
+        // find next possible paths
+        auto possiblePaths = moveAllDirection(m);
+         std::reverse(possiblePaths.begin(), possiblePaths.end());
+        
+        // possible path is not in set so add in stack
+        for(auto& i: possiblePaths){
+            std::string temp = transformArrayToString(i.first);
+            if(set.find(temp) == set.end()){
+                s.push(i);
+                set.insert(transformArrayToString(i.first));
+                
+            }
+        }
+        
+        // update maximum size of fridge if current stack size is bigger than previous
+        if(this->maximumSizeOfFringe < s.size())
+            maximumSizeOfFringe = s.size();
+    }
+    
+    std::cout << (solutionFind ? "DFS Solution find for ": "DFS Solution not find for ") << *this <<  " Nodes expanded: " << nodesExpanded << " Maximum Size of Fringe: " << maximumSizeOfFringe << " Depth: " << depth << "\n";
+    nodesExpanded = 0;
+    maximumSizeOfFringe = 0;
+}
+
+
+
+
+void _8puzzle::DLS(int depth, bool IDS){
+    std::stack<std::pair<std::array<int, 9>, int>> s; // stack for DFS
     std::unordered_set<std::string> set; // keep visited list
     bool solutionFind = false;
     
     s.push(this->currentState);
     
     while (!s.empty()) {
-        std::array<int, 9> m = s.top();
+        std::pair<std::array<int, 9>, int> m = s.top();
         s.pop();
+        /*
+        // depth control part
+        if(m.second - 1 >= depth){
+            continue;
+        }
         
-        set.insert(transformArrayToString(m));
-        if(std::equal(std::begin(m), std::end(m), std::begin(goalState))){
+        */
+        
+        // check if goal state reached
+        set.insert(transformArrayToString(m.first));
+        if(std::equal(std::begin(m.first), std::end(m.first), std::begin(goalState))){
             solutionFind = true;
+            std::cout << (IDS ? "IDS":"DLS") << (solutionFind ? " Solution find for ": " Solution not find for ") << *this <<  " Nodes expanded:" << nodesExpanded << " Maximum Size of Fringe:" << maximumSizeOfFringe << " Depth:" << m.second << " Iterative Depth:" << depth << "\n";
             break;
         }
         
+        // find next possible paths
         auto possiblePaths = moveAllDirection(m);
          std::reverse(possiblePaths.begin(), possiblePaths.end());
+        
         // possible path is not in set so add in stack
         for(auto& i: possiblePaths){
-            std::string temp = transformArrayToString(i);
+            // depth control part
+            if(i.second - 1 >= depth){
+                continue;
+            }
+            
+            std::string temp = transformArrayToString(i.first);
             if(set.find(temp) == set.end()){
                 s.push(i);
-                set.insert(transformArrayToString(i));
+                set.insert(transformArrayToString(i.first));
                 
             }
         }
+        
+        // update maximum size of fridge if current stack size is bigger than previous
         if(this->maximumSizeOfFringe < s.size())
             maximumSizeOfFringe = s.size();
     }
     
-    std::cout << (solutionFind ? "Solution find for ": " Solution not find for ") << *this <<  " Nodes expanded: " << nodesExpanded << " Maximum Size of Fringe: " << maximumSizeOfFringe << "\n";
+
+    if(!solutionFind)
+        std::cout << (IDS ? "IDS":"DLS") << " Solution not find for " << *this << " Depth: " << depth <<"\n";
+    currentState.second = 0;
     nodesExpanded = 0;
     maximumSizeOfFringe = 0;
 }
 
 
+void _8puzzle::IDS(int depth){
+    for(int i = 0; i <= depth; i++){
+        DLS(i, true);
+    }
+}
 
 
 void _8puzzle::randomProblemGenerator(){
@@ -204,7 +308,7 @@ void _8puzzle::randomProblemGenerator(){
         }
     }
     
-    this->currentState = m;
+    this->currentState.first = m;
 }
 
 
@@ -212,34 +316,35 @@ void _8puzzle::randomProblemGenerator(){
 
 // -----------------------------------------PRIVATE MEMBER FUNCTIONS---------------------------------------------------
 
-std::array<int, 9> _8puzzle::move(std::array<int, 9> currentState, int direction){
-    std::array<int, 9> m(currentState);
-    auto it = std::find(currentState.begin(), currentState.end(), 0);
-    auto index = it - currentState.begin();
+std::pair<std::array<int, 9>, int> _8puzzle::move(std::pair<std::array<int, 9>, int> currentState, int direction){
+    std::pair<std::array<int, 9>, int> m(currentState);
+    auto it = std::find(currentState.first.begin(), currentState.first.end(), 0);
+    auto index = it - currentState.first.begin();
+    m.second++;
     
     assert(direction > 0 && direction < 5 && "INVALID MOVE");
     
     switch (direction) {
         case 1: // up
             if(index > 2){
-                std::swap(m[index], m[index-3]);
+                std::swap(m.first[index], m.first[index-3]);
             }
             return m;
             
         case 2: // down
             if(index < 6){
-                std::swap(m[index], m[index+3]);
+                std::swap(m.first[index], m.first[index+3]);
             }
             return m;
             
         case 3: // left
             if(index % 3 > 0){
-                std::swap(m[index], m[index-1]);
+                std::swap(m.first[index], m.first[index-1]);
             }
             return m;
         case 4: // right
             if(index % 3 < 2){
-                std::swap(m[index], m[index+1]);
+                std::swap(m.first[index], m.first[index+1]);
             }
             return m;
             
@@ -253,9 +358,9 @@ std::array<int, 9> _8puzzle::move(std::array<int, 9> currentState, int direction
 
 
 
-std::vector<std::array<int, 9>> _8puzzle::moveAllDirection(std::array<int, 9> currentState){
+std::vector<std::pair<std::array<int, 9>, int>> _8puzzle::moveAllDirection(std::pair<std::array<int, 9>, int> currentState){
     // expand nodes
-    std::vector<std::array<int, 9>> paths;
+    std::vector<std::pair<std::array<int, 9>, int>> paths;
     for (int i = 1; i != 5; i++) {
         auto arr = move(currentState, i);
         if(arr == currentState)
